ddf77b0c4f98e097fd09f75f86b07608ad5d2a63
santhosh <santhoshmanoharan102001@gmail.com>
2023-09-14 00:58:54 +0530

    Front page

--- model/textModel.js
+++ model/textModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const textSchema = new Schema({
-    text:{type:String,require:true}
+    text:{type:String,require:true},
+    lang : {type:String,enum:['Python','Java','C'],require:true},
+    fname : {type : String,require:true}
 })
 
 module.exports=mongoose.model('text',textSchema)
